881eee5a66a085684e1dfc35d4fe7ee5290b500f
Rahully <yadavrahul.ry110@gmail.com>
2017-11-03 23:37:35 +0530

    First Commit

---
+++ Intv.java
@@ -0,0 +1,131 @@
+what is new features in java 7 and Java 8 (She mentioned that she worked in JDK 8 SO i asked basics question )
+What is lammbda expression , How it works , Can you write a basics program to explain?
+what is runtime polimorphish and compile time polmorphism , please write a program ? (Then i asked cross question )
+How inner class works in java ?
+What is wrapper class , please write a wrapper class ?
+What is Serialization and Deserialization?
+what is Java Stream API for collection classes , Can you explain internal working of steam api ?
+what is object class. explain in detail ?
+What is difference between Executor.submit() and Executer.execute() method ?
+how Can i make array volatile in Java?
+What is false sharing in the context of multi-threading?
+What is busy spin? Why should you use it?
+please write a programe to define producer and consumer problem in java ?
+How do you convert bytes to long in Java?
+What will this return 6*0.1 == 0.6? true or false?
+what is WeakReference and SoftReference in Java ?
+Difference between poll() and remove() method?
+Can we use random numbers in the hashcode() method?
+I have List of employee , what are the different ways to sort this list . and if I wanted to short the Employee list on the basis of age , Please write a program
+The difference between ByteBuffer and StringBuffer in Java?
+How do you convert a String(YYYYMMDD) to date in Java?
+How to write LRU cache in Java using Generics?
+Supose i have applied for a loan , And I wanted to reject the loan application , But i wanted to Give the reason list to Admin Person , Can you explain how to design admin tool for this ?
+Some quoestion on Try catch block , and Exception handling
+Check if a grid can become row-wise and column-wise sorted after adjacent swaps ?
+write a program to Check if a grid can become row-wise and column-wise sorted after adjacent swaps ?
+What is .dll ?how it is loaded? Why dynamic linking not static linking
+How to find k frequent words in a file ?
+Find largest word in dictionary by deleting some characters of given string ?
+How coursers works in Oracle ,
+Write a Store procedures , to transfer the data in a different database , But Please check if 2nd database table doesn't exists throw an error that 'Table doesn't exists'
+Given a word and a text, return the count of the occurences of anagrams of the word in the text(For eg: anagrams of word for are for, ofr, rof etc
+
+
+
+
+
+I have attended the interview recently in delloite for java profile. They were looking for 3–5 years of experience guys.
+
+So below are the set of questions they asked me. Again it depends upon on the panel you will get for interview but for me below questikns were asked.
+
+Round 1. Oline test Mcq
+
+Mostly from core Java (string.,interface,polymorphism , collection)
+
+Few from Spring and Hibernate .
+
+30 questions in 30 min. No negative marking.
+
+Round 2: f2f technical
+
+Immutable class
+Singleton class
+Hash code and equals method
+Hash map internal implementation.
+Difference between hash map and table.
+Iterate a hash map.
+Dependency injection concept.
+MVC architecture.
+My project details.
+Simple SQL query for getting max sal and no of employees having that sal in single query.
+Round 3:f2f technical
+
+Project details.
+DI in depth.
+Hibernate concepts.
+JDBC spring template.
+Why I want to switch.
+It was not that tough. :)
+
+Round 4: versant test
+
+Telephonic round where you have to answer a robots questions. Like you have to rrepeate a sentence , speak for 20 sec on a given topic.
+
+Very simple stuff.
+
+I have cleared all these rounds and now waiting for managerial round. Panels were not available that time so they asked me to wait till next week. Lets hope I clear that also.
+
+After that there is just HR round.
+
+I will update you once I attend them. \U0001f60a
+
+1st Feb 2017: (Managerial Round)
+Today i got call from HR for my Managerial Round . I have the option to take this from Home or from Deloitte office. I choose to attend this from home. It was a video conference call. HR shared the link for video Conferencing.
+
+Basically they will ask you behavior question in this round. But some managers ask some technical question also so be prepared.
+
+For me below question were asked :
+
+tell me about your academics and employment.
+Describe your role in current project .
+What all design pattern i have worked with.
+what is encapsulation .
+what is interface and abstract class . When to use what.
+What is your career aspiration.
+Why Deloitte
+Your Current CTC and Expected CTC
+Do you have any questions.
+I answered all of these questions. After some time i got a call from HR that i have cleared it. Will come back to you for further steps.
+
+May be there is just a HR round left and it will be telephonic .
+
+I will update the details once i received the call from them.
+
+Thanks. and Best of Luck.
+
+8th FEB 2017:
+
+Ok so yesterday i got call from HR for 1 last time. She told me that i have successfully cleared all the rounds and we want to offer you . After that she asked me few questions like notice period , total exerience, expected ctc, why that much , location .
+
+And day after that i got salary breakup mail from HR. They offered me more than what i have asked . The office formal letter they will send by next week.
+
+So this is the complete story of me and deloitte . Hope this will help someone in future as i was also looking for detailed information about interview process.
+
+The most important thing for deloitte is ur confident . If you answer them 70% with confidence and ur communication if above avg you can clear all these round.
+
+Best of luck .!!!
+
+
+
+Question::How to sort objects based on one of it's variables?
+Answer::Using Comparator interface of Collection.
+
+Question::GenericServlet vs HTTPServlet?
+Answer::
+
+Question::Given multiple catch blocks, which catch block would be executed. How to display error in jsp page using jsp tags?
+Anawer::
+
+Question::Find 2nd and 5th highest salary from table ?
+Answer::
